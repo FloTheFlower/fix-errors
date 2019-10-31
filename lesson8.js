2590e007c33d1b8f558455614291ed51e98bc606
FloTheFlower <florentina.hofbauer@gmail.com>
2019-10-30 20:19:07 -0700

    let age..

--- lesson8.js
+++ lesson8.js
@@ -1,5 +1,6 @@
 //Desired result Print out all people under age 50
 
+
 var people = [
     { name: "John", age: 55 },
     { name: "Roger", age: 44 },
@@ -8,10 +9,12 @@ var people = [
 
 var nameFilter = function() {
     for (var i = 0; i < people.length; i++) {
-        let name = people[i].name
-        if (name > 50) {
+        let name = people[i].name;
+        let age = people[i].age;
+        if (age < 50) {
             console.log("My Name is " + name)
         }
     }
 }
-nameFilter()
+
+nameFilter();
