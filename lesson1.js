455450f975f4793ed89ca93bc1ec3c456429709b
FloTheFlower <florentina.hofbauer@gmail.com>
2019-10-30 19:55:52 -0700

    yeah

--- lesson1.js
+++ lesson1.js
@@ -1,8 +1,7 @@
 //Should console.log all names in the name array
-
-var names [] = ["Pedro", "Marilyn"]
+var names = ["pedro", "john"]
 
 for (var i = 0; i < names.length; i++) {
     console.log("My Name is " + names[i])
 }
-let names = ["pedro", "john"]
+
